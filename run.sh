e4eafdd56cfcf9931507f88a951043b767f3598c
yezhejack <yezhejack@gmail.com>
2017-12-01 12:28:02 +0000

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python BiLSTM.py --embedding_path ~/data/GoogleNews-vectors-negative300.bin --isBinary --embedding_freeze --epoches 30
+python BiLSTM.py --hidden_size 600 --embedding_path ~/data/GoogleNews-vectors-negative300.bin --isBinary --embedding_freeze --epoches 30
